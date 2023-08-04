a42c9582fc958d5b6b376269091fd69558be1ff8
John <116607327+marsianjohncarter@users.noreply.github.com>
2023-08-04 10:58:58 -0700

    Update add.js

--- js/add.js
+++ js/add.js
@@ -1,5 +1,5 @@
 'use strict';
-import {fs} from '/fs';
+const fs = require('fs');
 fs.writeFile('output.txt', 'Hello World.', err => {
     if (err) console.error(err);
     else console.log('Data written to file successfully.');
