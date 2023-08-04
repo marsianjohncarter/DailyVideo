2afa8986e5219bf56ad205fed247ceb8e3efcda2
John <116607327+marsianjohncarter@users.noreply.github.com>
2023-08-04 10:27:34 -0700

    Update search.js

--- js/search.js
+++ js/search.js
@@ -36,7 +36,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     return await res.json();
   };
-  let videoData = getResource('https://marsianjohncarter.github.io/DailyVideo/videoDB.json/videos')
+  let videoData = getResource('https://marsianjohncarter.github.io/DailyVideo/videoDB.json')
     .then(data => {
       videos = data;
     });
